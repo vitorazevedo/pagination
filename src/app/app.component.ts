42b5fa8c1d80baa69cfbfaa2ec038f52c9709fd7
Vitor Azevedo <vitorazevedo@users.noreply.github.com>
2023-10-09 17:44:54 +0000

    changed from css to scss

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
   arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
